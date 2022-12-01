1eb61cd800c74b771633492374970b37132be9c6
GlobuloBianco <wufrancescojh@gmail.com>
2022-12-01 15:18:30 +0100

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { getPosts } from './posts.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    template: `
+  <app-navbar></app-navbar>
+  <router-outlet></router-outlet>
+  `,
+    styles: [``]
 })
 export class AppComponent {
-  title = 'S10-Day4';
+    constructor() {
+
+    }
 }
---
+++ src/app/pages/active-posts.page.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../models/post';
+import { getPosts } from '../posts.service';
+
+//let arrayAggiornato: object[] = [];
+
+@Component({
+    template: `
+    <div class="container mt-5">
+      <div *ngFor="let post of posts">
+        <div *ngIf="post.active"
+        [ngClass]="{
+      'bg-warning': post.type == 'news',
+      'bg-info': post.type == 'education',
+      'bg-dark': post.type == 'politic',
+      'text-white': post.type == 'politic'
+    }"
+    class="card mb-4">
+          <h5 class="card-header">Post</h5>
+          <div class="card-body">
+            <h5 class="card-title">{{ post.title }}</h5>
+            <p class="card-text">
+              {{ post.body }}
+            </p>
+            <button type="button" class="btn btn-primary" (click)="disattiva(post.id)">Disattiva</button>
+            <button type="button" class="btn btn-primary" (click)="cancella(post.id)">Cancella</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  `,
+    styles: [],
+})
+
+export class ActivePostsPage implements OnInit {
+    posts!: Post[];
+    constructor() {
+        getPosts().then((posts) => {
+            this.posts = posts.lista;
+            console.log(this.posts);
+            //arrayAggiornato = this.posts;
+        });
+    }
+
+    ngOnInit(): void {
+        //console.log(arrayAggiornato)
+    }
+
+    disattiva = (element: number) => {
+        //this.posts[element - 1].active = false;
+        console.log(this.posts)
+        //aggiornaJson
+        fetch('http://localhost:3000/lista' + '/' + element, {
+            method: 'PATCH',
+            body: JSON.stringify({
+                active: false,
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+    }
+
+    cancella = (element: number) => {
+        fetch('http://localhost:3000/lista' + '/' + element, {
+            method: 'DELETE',
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+    }
+}
